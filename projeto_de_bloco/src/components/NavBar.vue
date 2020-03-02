c040e7897b724a28d561341dd3c9a1fd62713d3a
Tiago Marques <ttsmarques@gmail.com>
2020-03-02 08:39:58 -0300

    organizando diretório

---
+++ projeto_de_bloco/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">Mega Filmes</b-navbar-brand>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Logar</b-nav-item>
+          <b-nav-item href="#">Registrar</b-nav-item>
+          </b-navbar-nav>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
